ba142a38038657232602d1c3c3e5c6e8c9b8b2f7
Yachna Patel <yachnapatel3010@gmail.com>
2025-03-20 15:22:43 -0400

    Initial commit for Frontend

---
+++ src/App.jsx
@@ -0,0 +1,67 @@
+import { useContext, useEffect } from "react";
+import "./App.css";
+import { Context } from "./main";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./components/Auth/Login";
+import Register from "./components/Auth/Register";
+import { Toaster } from "react-hot-toast";
+import axios from "axios";
+import Navbar from "./components/Layout/Navbar";
+import Footer from "./components/Layout/Footer";
+import Home from "./components/Home/Home";
+import Jobs from "./components/Job/Jobs";
+import JobDetails from "./components/Job/JobDetails";
+import Application from "./components/Application/Application";
+import MyApplications from "./components/Application/MyApplications";
+import PostJob from "./components/Job/PostJob";
+import NotFound from "./components/NotFound/NotFound";
+import MyJobs from "./components/Job/MyJobs";
+
+const App = () => {
+  const { setIsAuthorized, setUser } = useContext(Context);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:4000/api/v1/user/getuser",
+          { withCredentials: true }
+        );
+
+        if (response?.data?.user) {
+          setUser(response.data.user);
+          setIsAuthorized(true);
+        } else {
+          setIsAuthorized(false);
+        }
+      } catch (error) {
+        console.error("Error fetching user:", error);
+        setIsAuthorized(false);
+      }
+    };
+
+    fetchUser();
+  }, [setIsAuthorized, setUser]); // ✅ Added necessary dependencies
+
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/job/getall" element={<Jobs />} />
+        <Route path="/job/:id" element={<JobDetails />} />
+        <Route path="/application/:id" element={<Application />} />
+        <Route path="/applications/me" element={<MyApplications />} />
+        <Route path="/job/post" element={<PostJob />} />
+        <Route path="/job/me" element={<MyJobs />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
+      <Toaster />
+    </BrowserRouter>
+  );
+};
+
+export default App;
